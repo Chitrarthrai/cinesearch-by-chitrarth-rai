00ae08c90ab1b1b71ee22bdf344adca222855fc7
Chitrarth <chitrarthrai10@gmail.com>
2024-09-11 16:30:08 +0530

    added some fixes...staged commits for pervious files

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,13 +1,17 @@
-const reportWebVitals = onPerfEntry => {
+const reportWebVitals = async onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    // eslint-disable-next-line promise/prefer-await-to-then
-    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    try {
+      const { getCLS, getFID, getFCP, getLCP, getTTFB } = await import(
+        "web-vitals"
+      );
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
       getLCP(onPerfEntry);
       getTTFB(onPerfEntry);
-    });
+    } catch (error) {
+      console.error("Failed to load web-vitals:", error);
+    }
   }
 };
 
